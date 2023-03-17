0b26724ac8253cc99a90687c1071b1282783b92c
RJ Smith <rj@rjsmith.me>
2023-03-17 00:53:33 -0400

    Added neovim and wezterm configs.

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font 'Hack Nerd Font Mono',
+  font_size = 20.0,
+  color_scheme = 'Gruvbox Dark',
+  window_background_image = '/Users/rjsmith/.config/wallhaven-5gzdv8.jpg',
+  window_background_opacity = 0.15,
+}
